73f482a7254e500c9d7fdf4274ab607ed165a404
arshad <call8arshad@gmail.com>
2019-08-05 17:26:32 +0530

    Create main.js
    Given string is a pangram or not

---
+++ Pangrams/main.js
@@ -0,0 +1,48 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the pangrams function below.
+function pangrams(s) {
+    s = s.toUpperCase(); //convert the string into one case eith uper or lower
+    s = s.split(' ').join(''); // remove all the spaces
+    s = s.split('');    // create all char array.
+    s = [...new Set(s)]; // remove duplicate character from the array.
+
+    if (s.length === 26) return "pangram";
+    else return "not pangram"
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const s = readLine();
+
+    let result = pangrams(s);
+
+    ws.write(result + "\n");
+
+    ws.end();
+}
