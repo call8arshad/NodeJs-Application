6bcbc3ed78f17a5575e2d8821ba1c8c0e8722350
Arshad <arshad.k@aprecomm.ai>
2024-04-29 11:32:17 +0530

    QR Generator app support qr code generation for url, string, and file

--- qrCodeGenerator/README.md
+++ qrCodeGenerator/README.md
@@ -5,7 +5,7 @@ This Application will generate a QRCode for the Given String/JSON payload
 
 ## How to Run
 ```
-OUTPUT_PATH=path/outputFile INPUT='This is test String' node index.js
+FILE_WITH_PATH=path/outputFile  OUTPUT_FORMAT=file INPUT='This is test String' node index.js
 ```
 
 ## Input consists of a string s
--- qrCodeGenerator/index.js
+++ qrCodeGenerator/index.js
@@ -2,7 +2,9 @@
 
 const QRCode  = require('qrcode');
 
-let input     = process.env.INPUT;
+let input             = process.env.INPUT;
+const OUTPUT_FORMAT   = process.env.OUTPUT_FORMAT;
+const FILE_WITH_PATH  = process.env.OUTPUT_FILE || '/tmp/qrTest.png';
 
 if (!input) input = JSON.stringify({
   name: 'Arshad Khan',
@@ -12,11 +14,36 @@ if (!input) input = JSON.stringify({
 
 
 // Generate QRCode and print to the console
-function genQR(input) {
-  QRCode.toDataURL(input, (err, resp) => {
-    console.log(`QR Code for input: ${input}`, resp)
-  })
+function genQR(input, output) {
+  const _done = (err, resp, apiName) => {
+    if (err) throw err;
+    console.log(`${apiName}:: QR Code for input: <${input}> ::`, resp || 'generated');
+  }
+
+  switch(output) {
+    case 'url':
+      QRCode.toDataURL(input, (err, url) => {
+        _done(err, url, 'DataURL');
+      });
+      break;
+    case 'string':
+      QRCode.toString(input,  (err, string) => {
+        _done(err, string, 'String');
+      });
+      break;
+    case 'file' :
+      QRCode.toFile(FILE_WITH_PATH, input, {
+        errorCorrectionLevel: 'H',
+        color: {
+          // dark: '#00F',  // Blue dots
+          dark: '#000',  // Black dots
+          light: '#0000' // Transparent background
+        }
+      },  (err) => {
+        _done(err, null, 'File');
+      });
+  }
 }
 
 
-genQR(input);
+genQR(input, OUTPUT_FORMAT);
